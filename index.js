2ffc5cac5b3ba63ef7bce0fbbd411646731cbb09
trueuser <83166512+istrueuser@users.noreply.github.com>
2023-09-26 19:57:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ app.all('*', (req, res) => {
 
 app.use((_, res) => {});
 
-app.listen(env.PORT, "0.0.0.0", () => {
-  console.log("Live on port" + env.PORT);
+app.listen(process.env.PORT, "0.0.0.0", () => {
+  console.log("Live on port" + process.env.PORT);
 });
