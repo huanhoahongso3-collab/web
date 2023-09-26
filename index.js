5af6c276c5de45fc73307d32b40e9a2e8c84fc73
trueuser <83166512+istrueuser@users.noreply.github.com>
2023-09-26 16:51:49 +0700

    Addition

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const bodyParser = require('body-parser');
+const Database = require("@replit/database");
+
+const db = new Database();
+
+const compression = require('compression');
+app.use(compression());
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname + "/public"));
+
+app.all('*', (req, res) => {
+  res.status(404).sendFile(__dirname + "/404.html");
+});
+
+app.use((_, res) => {});
+
+app.listen(8080, () => {
+  console.log("Express app listening on port 8080");
+});
---
+++ public/gtag.js
@@ -0,0 +1,9 @@
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-PN9F3D1WT1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-PN9F3D1WT1');
+</script>
