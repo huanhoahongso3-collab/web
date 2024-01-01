d96c9c1e0dfe7ab4b2df08720f4a9c7636c092c6
trueuser <83166512+istrueuser@users.noreply.github.com>
2024-01-01 12:19:17 +0700

    cleanup

--- public/gtag.js
+++
@@ -1,9 +0,0 @@
-
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-PN9F3D1WT1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-PN9F3D1WT1');
-</script>
